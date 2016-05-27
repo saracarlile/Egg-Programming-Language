986f03393968c113124dc41474f058800eb17b14
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-05-27 13:44:57 -0500

    exercise 1

--- Egg-Programming-Language.js
+++ Egg-Programming-Language.js
@@ -142,6 +142,18 @@ topEnv["print"] = function(value) {
   return value;
 };
 
+topEnv["array"] = function(args){
+     return Array.prototype.slice.call(arguments, 0);    
+};
+
+topEnv["length"] = function(array) {
+  return array.length;
+};
+
+
+topEnv["element"] = function(array, i) {
+  return array[i];
+};
 //seventh code block
 
 function run() {
